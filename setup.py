18b4ab4c2e2ce30182f19e05a9983748a90a4eed
pulakeshpradhan <75819626+pulakeshpradhan@users.noreply.github.com>
2026-02-13 10:39:03 +0530

    Update author, email, and repo URL in setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,12 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name="opengeo",
     version="0.0.1",
-    author="OpenGeo Team",
+    author="Pulakesh Pradhan",
+    author_email="pulakesh.mid@gmail.com",
     description="A Google Earth Engine-like Python package using Xarray and Dask",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/example/opengeo",
+    url="https://github.com/pulakeshpradhan/opengeo",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
